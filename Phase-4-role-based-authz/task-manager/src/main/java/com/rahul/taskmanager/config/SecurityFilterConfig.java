532f1931b9bbd048e6708b8e459064e0a2554808
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-17 11:14:36 +0530

    Added base class and admin controller

---
+++ Phase-4-role-based-authz/task-manager/src/main/java/com/rahul/taskmanager/config/SecurityFilterConfig.java
@@ -0,0 +1,22 @@
+package com.rahul.taskmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityFilterConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> {})
+                .csrf((csrf) -> csrf.disable());  //this will enable all other endpoints than get from postman
+
+        return http.build();
+    }
+}
---
+++ Phase-4-role-based-authz/task-manager/src/main/java/com/rahul/taskmanager/dto/TaskRequest.java
@@ -0,0 +1,16 @@
+package com.rahul.taskmanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskRequest {
+    private String title;
+    private String description;
+    private boolean completed;
+}
