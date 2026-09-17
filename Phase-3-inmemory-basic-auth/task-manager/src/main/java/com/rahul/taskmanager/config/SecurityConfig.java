451c88f95255857856b0f875052c42a9e12eb9ae
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-17 10:51:27 +0530

    User has been added

--- Phase-3-inmemory-basic-auth/task-manager/src/main/java/com/rahul/taskmanager/config/SecurityConfig.java
+++ Phase-3-inmemory-basic-auth/task-manager/src/main/java/com/rahul/taskmanager/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.rahul.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,28 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        var alice = User.builder()
+                .username("Ajay")
+                .password(passwordEncoder.encode("password123"))
+                .roles("USER")
+                .build();
+
+        var bob = User.builder()
+                .username("Amit")
+                .password(passwordEncoder.encode("password123"))
+                .roles("USER")
+                .build();
+
+        var admin = User.builder()
+                .username("Rahul")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(alice, bob, admin);
     }
 }
