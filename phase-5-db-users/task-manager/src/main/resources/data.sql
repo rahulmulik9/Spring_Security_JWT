43dacd8703f3e817a8ea84bde523df86f0f54e33
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-17 11:46:15 +0530

    Added User into database

--- phase-5-db-users/task-manager/src/main/resources/data.sql
+++ phase-5-db-users/task-manager/src/main/resources/data.sql
@@ -1,3 +1,8 @@
+INSERT INTO users (username, password, role) VALUES
+('Ajay', '$2b$10$.8fP4TPs0lF1pAfFR4X.3u0YyIbninbdfP9n0lWKiFe5Q2FVwC3/e', 'USER'),
+('Rahul', '$2b$10$KXM3fu.YdxthgmYU86bTReXUU00B5Gt3DCvwi3e.ovVho3lWnP/jG', 'ADMIN');
+
+
 INSERT INTO tasks (title, description, completed, created_at) VALUES
 ('Buy groceries', 'Milk, eggs, bread, butter', false, now()),
 ('Finish Spring Security plan', 'Work through Phase 1 to Phase 10', false, now()),
