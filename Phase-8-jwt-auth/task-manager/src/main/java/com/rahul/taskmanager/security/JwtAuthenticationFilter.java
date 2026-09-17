c6fc2a75cccc99106d82ddc09adede3e24fabe90
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-17 16:40:59 +0530

    Access authorities from JWT toke and username then created UsernamePasswordAuthenticationToken

--- Phase-8-jwt-auth/task-manager/src/main/java/com/rahul/taskmanager/security/JwtAuthenticationFilter.java
+++ Phase-8-jwt-auth/task-manager/src/main/java/com/rahul/taskmanager/security/JwtAuthenticationFilter.java
@@ -5,10 +5,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -27,13 +32,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        String token = authHeader.substring(7); // strip "Bearer " prefix
+        String token = authHeader.substring(7);
 
         if (jwtUtil.isTokenValid(token)) {
             String username = jwtUtil.extractUsername(token);
             String role = jwtUtil.extractRole(token);
 
-            // SecurityContextHolder wiring comes in substep 3.2
+            //get authority from JWT token
+            List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+
+            //create UsernamePasswordAuthenticationToken using name and authorities
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
